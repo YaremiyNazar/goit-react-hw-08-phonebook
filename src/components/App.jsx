c260f1f2a9e55f873aab9dc54022d2c55dbd7d1a
Your Name <yaremiy.nazar@gmail.com>
2023-03-24 00:09:40 +0200

    hw_8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import  {fetchContacts}  from 'redux/contactSlice/contactsOperations';
+
+import ContactList from '../components/ContactList/ContactList';
+import ContactForm from '../components/ContactForm/ContactForm';
+import Filter from '../components/Filter/Filter';
+
+import css from '../components/App.module.css';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.wrapper}>
+      <div className={css.contact__form}>
+        <h1>PhoneBook</h1>
+        <ContactForm />
+      </div>
+      <div className={css.contact}>
+        <h2>Contacts</h2>
+        <Filter />
+        { <ContactList />}
+      </div>
     </div>
   );
 };
+export default App;
